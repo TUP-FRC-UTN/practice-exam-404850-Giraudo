3451e8e423af24049eed87f76a55e3fc91e968bd
404850-Giraudo <404850@tecnicatura.frc.utn.edu.ar>
2024-11-01 12:34:11 -0300

    creacion de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { OrderFormComponent } from './order-form/order-form.component';
+import { OrderListComponent } from './order-list/order-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'create-order', component: OrderFormComponent },
+    { path: 'orders', component: OrderListComponent },
+    { path: '', redirectTo: '/create-order', pathMatch: 'full'}
+];
